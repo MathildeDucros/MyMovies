21b1d67c50d1dcf3b5adac91ea0578b5e96384e7
Mathilde <ducros.mathilde@gmail.com>
2023-05-23 10:45:50 +0200

    last commit

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = "https://backend-my-moviz.vercel.app/";
+const BACKEND_URL = "https://my-movies-back.vercel.app/";
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = "https://frontend-my-moviz.vercel.app/";
+const FRONTEND_URL = "https://my-moviz-front-two.vercel.app/";
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
